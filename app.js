7ec86f2c2199b75f98ff20adc80b0cdd8d4725e8
Szűcs Dániel <daniel.szucs@stud.uni-obuda.hu>
2023-04-28 02:39:09 +0200

    Rewrite server connection

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv/config')
+
+app.use(bodyParser.json())
+
+// IMPORT ROUTES
+const postsRoute = require('./routes/posts')
+app.use('/posts', postsRoute)
 
 // CONNECT TO DB
 // Requires a .env file which contains the database connection string in a form like
 // DB_CONNECTION=connection123
-const client = new MongoClient(process.env.DB_CONNECTION, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-})
 
-async function run() {
-    try {
-        await client.connect()
-        await client.db('admin').command({ ping: 1 })
-        console.log('Ping was successful. You are connected to MongoDB')
-    } finally {
-        await client.close()
+async function connectToMongoDB() {
+    await mongoose.connect(process.env.DB_CONNECTION)
+    switch (mongoose.connection.readyState) {
+        case 0:
+            console.log('Disconnected')
+            break
+        case 1:
+            console.log('Connected')
+            break
+        case 2:
+            console.log('Connecting')
+            break
+        case 3:
+            console.log('Disconnecting')
+            break
     }
 }
 
-run().catch(console.dir)
-
-// MIDDLEWARES
-app.use('/posts', () => {
-    console.log('This is a middleware running')
-})
-
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('This it the root route')
-})
-
-app.get('/posts', (req, res) => {
-    res.send('This is a sub route')
-})
+connectToMongoDB().catch(console.dir)
 
 // SERVER LISTENING
 app.listen(3000)
